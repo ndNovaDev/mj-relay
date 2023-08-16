a5e5fcec7f35ddf49502a58aef4eb3f79bb82cbd
LiQingyang <liqingyang@xiaomi.com>
2023-08-16 15:28:11 +0800

    feat: mj status

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-from flask import Flask, jsonify, make_response, send_file
-import os
+from flask import Flask, jsonify
 import requests
-import io
 from PIL import Image
 from io import BytesIO
+from base64 import b64encode
+from os import getenv
 
-def serve_pil_image(pil_img):
-    img_io = BytesIO()
-    pil_img.save(img_io, 'JPEG', quality=70)
-    img_io.seek(0)
-    return send_file(img_io, mimetype='image/jpeg')
-    
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    
+@app.route('/mj/task/<id>')
+def get_task_status(id):
+    mj_status_url = getenv('mj.relay.domain', default='https://midjourney-proxy-production-77c1.up.railway.app') + '/mj/task/{id}/fetch'.format(id=id)
+    mj_status_response = requests.get(mj_status_url).json()
+    progress_str = mj_status_response['progress']
+    response = {
+        "status": mj_status_response['status'],
+        "progress": 0 if progress_str == '' else int(progress_str[:-1]),
+        "image": Null if mj_status_response['status'] != 'SUCCESS' else get_mj_image(mj_status_response['imageUrl']) 
+    }
+    return jsonify(response)
 
-@app.route('/imagine')
-def imagine():
-    image_url = 'https://cdn.discordapp.com/attachments/1140481534760067198/1140559999102353518/novadev__Phone_settings_button_Windows_Vista_style_solid_color__cb030d47-10ab-4185-8b33-e2d55a5f8640.png'
-    image_response = requests.get(image_url)
+def get_mj_image(mj_image_url):
+    image_response = requests.get(mj_image_url)
 
-    pil_ori = Image.open(io.BytesIO(image_response.content))
+    pil_ori = Image.open(BytesIO(image_response.content))
     (width, height) = pil_ori.size
     pil_cropped = pil_ori.crop((0, 0, width / 2, height / 2))
 
+    return pil_image_to_base64(pil_cropped)
 
-    return serve_pil_image(pil_cropped)
+def pil_image_to_base64(image):
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    return 'data:image/jpeg;base64,' + b64encode(buffered.getvalue()).decode('utf-8')
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(debug=True, port=getenv("PORT", default=3000))
+
